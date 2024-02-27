f040182527787b44263f5f62eb12651645808bcd
aggreyjr <tukashabaaggrey89@gmail.com>
2024-02-27 11:10:32 +0000

    1st

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Aggrey Tukashaba <https://github.com/aggreyjr:wq
+ * >
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
